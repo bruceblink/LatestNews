b6b193f2a9372a0323fe4378d0ff7fefa48e7c6f
likanug <likanug.g@qq.com>
2025-09-22 20:02:05 +0800

    feat: 使用dotenv-flow替代dotenv读取多个配置文件，实现多端配置文件共同管理

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,10 @@
 /// <reference types="vite-plugin-pwa/react" />
 /// <reference types="vite-plugin-pwa/info" />
 /// <reference lib="webworker" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
 import { join } from "node:path";
 import unocss from "unocss/vite";
 import { defineConfig } from "vite";
+import dotenvFlow from "dotenv-flow";
 import unimport from "unimport/unplugin";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
@@ -10,8 +10,9 @@ import pwa from "./pwa.config";
 import { projectDir } from "./shared/dir";
 import initVoteNitro from "./nitro.config";
 
-dotenv.config({
-  path: join(projectDir, ".env.server"),
+// 自动按 .env、.env.local、.env.development 等文件顺序加载
+dotenvFlow.config({
+  path: join(__dirname),
 });
 
 export default defineConfig({
