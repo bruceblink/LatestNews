7ca88131b1cb4589164f62fd3c3b992773882fc4
likanug <likanug.g@qq.com>
2025-09-22 16:26:20 +0800

    chore: 更新主页的footer.tsx中的应用名称和版权日期为可配置的变量

--- shared/consts.ts
+++ shared/consts.ts
@@ -8,6 +8,8 @@ export const TTL = 30 * 60 * 1000;
  * 默认刷新间隔, 10 min
  */
 export const Interval = 10 * 60 * 1000;
+// 项目名称
+export const APP_NAME = packageJSON.name;
 // GitHub项目地址
 export const ProjectUrl = packageJSON.projectUrl;
 // 应用部署地址
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,16 @@
-import {ProjectUrl} from "@shared/consts";
+import dayjs from "dayjs/esm";
+import { APP_NAME, ProjectUrl } from "@shared/consts";
 
 export function Footer() {
   return (
     <>
-        <a href={`${ProjectUrl}/blob/main/LICENSE`} target="_blank">
-            MIT LICENSE
-        </a>
+      <a href={`${ProjectUrl}/blob/main/LICENSE`} target="_blank">
+        MIT LICENSE
+      </a>
       <span>
-        <span>NewsNow © 2024 By </span>
+        <span>
+          {APP_NAME} © {dayjs().year()} By{" "}
+        </span>
         <a href={Author.url} target="_blank">
           {Author.name}
         </a>
