f8b24e4b5972a77c767b032a6fb2ebd71ff8eff8
likanug <likanug.g@qq.com>
2025-09-25 14:53:11 +0800

    chore: 格式化代码

--- server/getters.ts
+++ server/getters.ts
@@ -7,11 +7,7 @@ import type { SourceGetter } from "./types";
 export const getters = (function initGetters() {
     const _getters = {} as Record<SourceID, SourceGetter>;
     typeSafeObjectEntries(x).forEach(([id, v]) => {
-        if (v.default instanceof Function) {
-            Object.assign(_getters, { [id]: v.default });
-        } else {
-            Object.assign(_getters, v.default);
-        }
+        Object.assign(_getters, { [id]: v.default });
     });
     return _getters;
 })();
