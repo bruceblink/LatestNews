fbc2b89c5a79ba055b3b1eef4a8f7f06c6be6cfb
likanug <likanug.g@qq.com>
2025-09-27 12:27:09 +0800

    fix: 修复执行refresh.ts时报错"Error [ERR_MODULE_NOT_FOUND]: Cannot find package '@shared/consts.ts'"的问题

--- scripts/refresh.ts
+++ scripts/refresh.ts
@@ -1,5 +1,5 @@
-import { HOME_PAGE } from "@shared/consts.ts";
-
+import packageJSON from "../package.json";
 import sources from "../shared/sources.json";
+const HOME_PAGE = packageJSON.homepage;
 
 Promise.all(Object.keys(sources).map((id) => fetch(`${HOME_PAGE}/api/s?id=${id}`))).catch(console.error);
