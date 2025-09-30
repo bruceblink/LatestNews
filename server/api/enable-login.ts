637669d3cbb1369d4f5a706abca52f76a34eca65
likanug <likanug.g@qq.com>
2025-09-30 14:08:32 +0800

    refactor: "/api/enable-login"接口删除返回值url

--- server/api/enable-login.ts
+++ server/api/enable-login.ts
@@ -1,9 +1,7 @@
-import process from "node:process";
 import { defineEventHandler } from "h3";
 
 export default defineEventHandler(async () => {
     return {
         enable: true,
-        url: `https://github.com/login/oauth/authorize?client_id=${process.env.G_CLIENT_ID}`,
     };
 });
