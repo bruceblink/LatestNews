bd4418c038552805ae366a7a5b3a6bb6a02a152d
likanug <likanug.g@qq.com>
2025-09-30 14:07:37 +0800

    refactor: 删除无用的代码

--- server/api/login.ts
+++
@@ -1,6 +0,0 @@
-import process from "node:process";
-import { sendRedirect, defineEventHandler } from "h3";
-
-export default defineEventHandler(async (event) => {
-    await sendRedirect(event, `https://github.com/login/oauth/authorize?client_id=${process.env.G_CLIENT_ID}`);
-});
