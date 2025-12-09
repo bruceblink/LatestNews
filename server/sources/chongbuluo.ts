1dc1809e5328212534bc3b38ca85ed1e3a8f4efc
likanug <likanug.g@qq.com>
2025-12-09 16:26:29 +0800

    refactor: 更新"chongbuluo-hot"的数据源实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id生成方式

--- server/sources/chongbuluo.ts
+++ server/sources/chongbuluo.ts
@@ -2,7 +2,7 @@ import type { NewsItem } from "@shared/types";
 
 import * as cheerio from "cheerio";
 import { myFetch } from "#/utils/fetch";
-import { defineSource, defineRSSSource } from "#/utils/source";
+import { defineSource, defineRSSSource, generateUrlHashId } from "#/utils/source";
 
 const hot = defineSource(async () => {
     const baseUrl = "https://www.chongbuluo.com/";
@@ -10,18 +10,27 @@ const hot = defineSource(async () => {
     const $ = cheerio.load(html);
     const news: NewsItem[] = [];
 
-    $(".bmw table tr").each((_, elem) => {
+    const $items = $(".bmw table tr");
+
+    // 使用 for...of 循环替代 .each()，以便支持 await
+    for (const elem of $items) {
         const xst = $(elem).find(".common .xst").text();
         const url = $(elem).find(".common a").attr("href");
-        news.push({
-            id: baseUrl + url,
-            url: baseUrl + url,
-            title: xst,
-            extra: {
-                hover: xst,
-            },
-        });
-    });
+
+        if (url && xst) {
+            const fullUrl = url.startsWith("http") ? url : `${baseUrl}${url}`;
+            const hashId = await generateUrlHashId(fullUrl);
+
+            news.push({
+                id: hashId,
+                url: fullUrl,
+                title: xst,
+                extra: {
+                    hover: xst,
+                },
+            });
+        }
+    }
 
     return news;
 });
