01a1b606ccf7f9d9b6e2fc2e6c73fb0d60299c53
likanug <likanug.g@qq.com>
2025-12-10 20:02:20 +0800

    refactor: 更新"github-trending-today"数据源的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/github.ts
+++ server/sources/github.ts
@@ -3,33 +3,43 @@ import type { NewsItem } from "@shared/types";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 const trending = defineSource(async () => {
     const baseURL = "https://github.com";
     const html: any = await myFetch("https://github.com/trending?spoken_language_code=");
     const $ = cheerio.load(html);
     const $main = $("main .Box div[data-hpc] > article");
-    const news: NewsItem[] = [];
-    $main.each((_, el) => {
+
+    const newsTasks: Promise<NewsItem | null>[] = [];
+    for (const el of $main) {
         const a = $(el).find(">h2 a");
         const title = a.text().replace(/\n+/g, "").trim();
         const url = a.attr("href");
         const star = $(el).find("[href$=stargazers]").text().replace(/\s+/g, "").trim();
         const desc = $(el).find(">p").text().replace(/\n+/g, "").trim();
-        if (url && title) {
-            news.push({
-                url: `${baseURL}${url}`,
-                title,
-                id: url,
-                extra: {
-                    info: `✰ ${star}`,
-                    hover: desc,
-                },
-            });
-        }
-    });
-    return news;
+
+        if (!url || !title) continue;
+        const fullUrl = url.startsWith("http") ? url : `${baseURL}${url}`;
+
+        newsTasks.push(
+            (async () => {
+                const hashId = await generateUrlHashId(fullUrl);
+
+                return {
+                    id: hashId,
+                    title,
+                    url: fullUrl,
+                    extra: {
+                        info: `✰ ${star}`,
+                        hover: desc,
+                    },
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(newsTasks);
+    return results as NewsItem[];
 });
 
 export default defineSource({
