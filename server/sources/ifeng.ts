a1967968483d8baf813dd3110f2faebf09fbc494
likanug <likanug.g@qq.com>
2025-12-10 20:37:12 +0800

    refactor: 更新"ifeng"数据源的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/ifeng.ts
+++ server/sources/ifeng.ts
@@ -1,30 +1,32 @@
 import type { NewsItem } from "@shared/types";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 export default defineSource(async () => {
     const html: string = await myFetch("https://www.ifeng.com/");
     const regex = /var\s+allData\s*=\s*(\{[\s\S]*?});/;
     const match = regex.exec(html);
-    const news: NewsItem[] = [];
-    if (match) {
-        const realData = JSON.parse(match[1]);
-        const rawNews = realData.hotNews1 as {
-            url: string;
-            title: string;
-            newsTime: string;
-        }[];
-        rawNews.forEach((hotNews) => {
-            news.push({
-                id: hotNews.url,
-                url: hotNews.url,
-                title: hotNews.title,
-                extra: {
-                    date: hotNews.newsTime,
-                },
-            });
-        });
-    }
+
+    if (!match) return [];
+
+    const realData = JSON.parse(match[1]);
+    const rawNews = realData?.hotNews1 as {
+        url: string;
+        title: string;
+        newsTime: string;
+    }[];
+
+    const news: NewsItem[] = await Promise.all(
+        rawNews.map(async (hotNews) => ({
+            id: await generateUrlHashId(hotNews.url),
+            url: hotNews.url,
+            title: hotNews.title,
+            extra: {
+                date: hotNews.newsTime,
+            },
+        }))
+    );
+
     return news;
 });
