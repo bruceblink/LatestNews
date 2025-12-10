8dd36890aa48985f537199bc5645eefc41d504b4
likanug <likanug.g@qq.com>
2025-12-10 20:15:08 +0800

    refactor: 更新"hackernews"数据源的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/hackernews.ts
+++ server/sources/hackernews.ts
@@ -3,31 +3,41 @@ import type { NewsItem } from "@shared/types";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 export default defineSource(async () => {
     const baseURL = "https://news.ycombinator.com";
     const html: any = await myFetch(baseURL);
     const $ = cheerio.load(html);
     const $main = $(".athing");
-    const news: NewsItem[] = [];
-    $main.each((_, el) => {
+
+    const newsTasks: Promise<NewsItem | null>[] = [];
+
+    for (const el of $main) {
         const a = $(el).find(".titleline a").first();
         // const url = a.attr("href")
         const title = a.text();
         const id = $(el).attr("id");
         const score = $(`#score_${id}`).text();
         const url = `${baseURL}/item?id=${id}`;
-        if (url && id && title) {
-            news.push({
-                url,
-                title,
-                id,
-                extra: {
-                    info: score,
-                },
-            });
-        }
-    });
-    return news;
+
+        if (!url || !id || !title) continue;
+
+        newsTasks.push(
+            (async () => {
+                const hashId = await generateUrlHashId(url);
+
+                return {
+                    id: hashId,
+                    title,
+                    url,
+                    extra: {
+                        info: score,
+                    },
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(newsTasks);
+    return results as NewsItem[];
 });
