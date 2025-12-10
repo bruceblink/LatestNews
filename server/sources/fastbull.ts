eadfc2df37bb2bd5882ed73a7ad7b101acb53c50
likanug <likanug.g@qq.com>
2025-12-10 16:28:04 +0800

    refactor: 更新"fastbull-express"和"fastbull-news"数据源的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id生成方式

--- server/sources/fastbull.ts
+++ server/sources/fastbull.ts
@@ -3,7 +3,7 @@ import type { NewsItem } from "@shared/types";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 const express = defineSource(async () => {
     const baseURL = "https://www.fastbull.com";
@@ -11,21 +11,24 @@ const express = defineSource(async () => {
     const $ = cheerio.load(html);
     const $main = $(".news-list");
     const news: NewsItem[] = [];
-    $main.each((_, el) => {
+    for (const el of $main) {
         const a = $(el).find(".title_name");
         const url = a.attr("href");
         const titleText = a.text();
         const title = titleText.match(/【(.+)】/)?.[1] ?? titleText;
         const date = $(el).attr("data-date");
         if (url && title && date) {
+            const fullUrl = url.startsWith("http") ? url : `${baseURL}${url}`;
+            const hashId = await generateUrlHashId(fullUrl);
+
             news.push({
-                url: baseURL + url,
+                id: hashId,
                 title: title.length < 4 ? titleText : title,
-                id: url,
+                url: fullUrl,
                 pubDate: Number(date),
             });
         }
-    });
+    }
     return news;
 });
 
@@ -35,20 +38,23 @@ const news = defineSource(async () => {
     const $ = cheerio.load(html);
     const $main = $(".trending_type");
     const newsItems: NewsItem[] = [];
-    $main.each((_, el) => {
+    for (const el of $main) {
         const a = $(el);
         const url = a.attr("href");
         const title = a.find(".title").text();
         const date = a.find("[data-date]").attr("data-date");
         if (url && title && date) {
+            const fullUrl = url.startsWith("http") ? url : `${baseURL}${url}`;
+            const hashId = await generateUrlHashId(fullUrl);
+
             newsItems.push({
-                url: baseURL + url,
+                id: hashId,
                 title,
-                id: url,
+                url: fullUrl,
                 pubDate: Number(date),
             });
         }
-    });
+    }
     return newsItems;
 });
 
