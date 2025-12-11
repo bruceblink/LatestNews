c59d12c3c7862742931602ec2837935500e2911f
likanug <likanug.g@qq.com>
2025-12-11 14:29:46 +0800

    refactor: 更新数据源"solidot"实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/solidot.ts
+++ server/sources/solidot.ts
@@ -3,16 +3,16 @@ import type { NewsItem } from "@shared/types";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
 import { parseRelativeDate } from "../utils/date";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 export default defineSource(async () => {
     const baseURL = "https://www.solidot.org";
     const html: any = await myFetch(baseURL);
     const $ = cheerio.load(html);
     const $main = $(".block_m");
-    const news: NewsItem[] = [];
-    $main.each((_, el) => {
+    const news: Promise<NewsItem | null>[] = [];
+    for (const el of $main) {
         const a = $(el).find(".bg_htit a").last();
         const url = a.attr("href");
         const title = a.text();
@@ -24,14 +24,23 @@ export default defineSource(async () => {
             ?.replace(/[年月]/g, "-")
             .replace("时", ":")
             .replace(/[分日]/g, "");
-        if (url && title && date) {
-            news.push({
-                url: baseURL + url,
-                title,
-                id: url,
-                pubDate: parseRelativeDate(date, "Asia/Shanghai").valueOf(),
-            });
-        }
-    });
-    return news;
+
+        if (!url || !title || !date) continue;
+
+        news.push(
+            (async () => {
+                const fullUrl = `${baseURL}${url}`;
+                const hashId = await generateUrlHashId(fullUrl);
+
+                return {
+                    id: hashId,
+                    title,
+                    url: fullUrl,
+                    pubDate: parseRelativeDate(date, "Asia/Shanghai").valueOf(),
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(news);
+    return results as NewsItem[];
 });
