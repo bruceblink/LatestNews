cde72d174f3ffb0d3e02daa33655cefb273b4a58
likanug <likanug.g@qq.com>
2025-12-11 13:31:04 +0800

    refactor: 更新"ithome"数据源的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/ithome.ts
+++ server/sources/ithome.ts
@@ -3,31 +3,38 @@ import type { NewsItem } from "@shared/types";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
 import { parseRelativeDate } from "../utils/date";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 export default defineSource(async () => {
     const response: any = await myFetch("https://www.ithome.com/list/");
     const $ = cheerio.load(response);
     const $main = $("#list > div.fl > ul > li");
-    const news: NewsItem[] = [];
-    $main.each((_, el) => {
+    const newsTasks: Promise<NewsItem>[] = [];
+    for (const el of $main) {
         const $el = $(el);
         const $a = $el.find("a.t");
         const url = $a.attr("href");
         const title = $a.text();
         const date = $(el).find("i").text();
-        if (url && title && date) {
-            const isAd = url?.includes("lapin") || ["神券", "优惠", "补贴", "京东"].find((k) => title.includes(k));
-            if (!isAd) {
-                news.push({
-                    url,
+
+        if (!url || !title || !date) continue;
+        const isAd = url?.includes("lapin") || ["神券", "优惠", "补贴", "京东"].find((k) => title.includes(k));
+
+        if (isAd) continue;
+        newsTasks.push(
+            (async () => {
+                const hashId = await generateUrlHashId(url);
+
+                return {
+                    id: hashId,
                     title,
-                    id: url,
+                    url,
                     pubDate: parseRelativeDate(date, "Asia/Shanghai").valueOf(),
-                });
-            }
-        }
-    });
-    return news.sort((m, n) => (n.pubDate! > m.pubDate! ? 1 : -1));
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(newsTasks);
+    return results.sort((m, n) => (n?.pubDate! > m?.pubDate! ? 1 : -1));
 });
