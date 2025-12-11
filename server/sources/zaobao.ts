6550457875ca69e076ee977f69168271db34f281
likanug <likanug.g@qq.com>
2025-12-11 15:53:15 +0800

    refactor: 更新数据源"zaobao"的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/zaobao.ts
+++ server/sources/zaobao.ts
@@ -5,8 +5,8 @@ import * as cheerio from "cheerio";
 import { Buffer } from "node:buffer";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
 import { parseRelativeDate } from "../utils/date";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 export default defineSource(async () => {
     const response: ArrayBuffer = await myFetch("https://www.zaochenbao.com/realtime/", {
@@ -16,20 +16,29 @@ export default defineSource(async () => {
     const utf8String = iconv.decode(Buffer.from(response), "gb2312");
     const $ = cheerio.load(utf8String);
     const $main = $("div.list-block>a.item");
-    const news: NewsItem[] = [];
-    $main.each((_, el) => {
+    const news: Promise<NewsItem>[] = [];
+    for (const el of $main) {
         const a = $(el);
         const url = a.attr("href");
         const title = a.find(".eps")?.text();
         const date = a.find(".pdt10")?.text().replace(/-\s/g, " ");
-        if (url && title && date) {
-            news.push({
-                url: base + url,
-                title,
-                id: url,
-                pubDate: parseRelativeDate(date, "Asia/Shanghai").valueOf(),
-            });
-        }
-    });
-    return news.sort((m, n) => (n.pubDate! > m.pubDate! ? 1 : -1));
+
+        if (!url || !title || !date) continue;
+
+        news.push(
+            (async () => {
+                const fullUrl = `${base}${url}`;
+                const hashId = await generateUrlHashId(fullUrl);
+
+                return {
+                    id: hashId,
+                    title,
+                    url: fullUrl,
+                    pubDate: parseRelativeDate(date, "Asia/Shanghai").valueOf(),
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(news);
+    return results.sort((m, n) => (n?.pubDate! > m?.pubDate! ? 1 : -1));
 });
