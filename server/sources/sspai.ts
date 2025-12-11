16ef2c8331aa290b41b184d8d1437cf16d2d0066
likanug <likanug.g@qq.com>
2025-12-11 14:44:21 +0800

    refactor: 更新数据源"sspai"实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/sspai.ts
+++ server/sources/sspai.ts
@@ -1,5 +1,5 @@
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 interface Res {
     data: {
@@ -13,11 +13,16 @@ export default defineSource(async () => {
     const limit = 30;
     const url = `https://sspai.com/api/v1/article/tag/page/get?limit=${limit}&offset=0&created_at=${timestamp}&tag=%E7%83%AD%E9%97%A8%E6%96%87%E7%AB%A0&released=false`;
     const res: Res = await myFetch(url);
-    return res.data.map((k) => {
-        return {
-            id: k.id,
-            title: k.title,
-            url: `https://sspai.com/post/${k.id}`,
-        };
-    });
+    return await Promise.all(
+        res?.data.map(async (k) => {
+            const fulUrl = `https://sspai.com/post/${k.id}`;
+            const hashId = await generateUrlHashId(fulUrl);
+
+            return {
+                id: hashId,
+                title: k.title,
+                url: fulUrl,
+            };
+        })
+    );
 });
