7f4249d0385b0b623e90dc352481d7e9e6c35854
likanug <likanug.g@qq.com>
2025-12-11 14:40:45 +0800

    refactor: 更新数据源"sputniknewscn"实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/sputniknewscn.ts
+++ server/sources/sputniknewscn.ts
@@ -2,7 +2,7 @@ import type { NewsItem } from "@shared/types";
 
 import * as cheerio from "cheerio";
 import { HOME_PAGE } from "@shared/consts.ts";
-import { proxySource, defineSource } from "#/utils/source";
+import { proxySource, defineSource, generateUrlHashId } from "#/utils/source";
 
 import { myFetch } from "../utils/fetch";
 
@@ -10,25 +10,34 @@ const source = defineSource(async () => {
     const response: any = await myFetch("https://sputniknews.cn/services/widget/lenta/");
     const $ = cheerio.load(response);
     const $items = $(".lenta__item");
-    const news: NewsItem[] = [];
-    $items.each((_, el) => {
+    const news: Promise<NewsItem | null>[] = [];
+    for (const el of $items) {
         const $el = $(el);
         const $a = $el.find("a");
         const url = $a.attr("href");
         const title = $a.find(".lenta__item-text").text();
         const date = $a.find(".lenta__item-date").attr("data-unixtime");
-        if (url && title && date) {
-            news.push({
-                url: `https://sputniknews.cn${url}`,
-                title,
-                id: url,
-                extra: {
-                    date: new Date(Number(`${date}000`)).getTime(),
-                },
-            });
-        }
-    });
-    return news;
+
+        if (!url || !title || !date) continue;
+
+        news.push(
+            (async () => {
+                const fullUrl = `https://sputniknews.cn${url}`;
+                const hashId = await generateUrlHashId(fullUrl);
+
+                return {
+                    id: hashId,
+                    title,
+                    url: fullUrl,
+                    extra: {
+                        date: new Date(Number(`${date}000`)).getTime(),
+                    },
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(news);
+    return results as NewsItem[];
 });
 
 export default proxySource(`${HOME_PAGE}/api/s?id=sputniknewscn&latest=`, source);
