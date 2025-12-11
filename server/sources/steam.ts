144260a7c9357021cb89cff22a3db796c53fe263
likanug <likanug.g@qq.com>
2025-12-11 14:54:16 +0800

    refactor: 更新数据源"steam"实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/steam.ts
+++ server/sources/steam.ts
@@ -3,34 +3,40 @@ import type { NewsItem } from "@shared/types.ts";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch.ts";
-import { defineSource } from "../utils/source.ts";
+import { defineSource, generateUrlHashId } from "../utils/source.ts";
 
 export default defineSource(async () => {
     const response: any = await myFetch("https://store.steampowered.com/stats/stats/");
     const $ = cheerio.load(response);
     const $rows = $("#detailStats tr.player_count_row");
-    const news: NewsItem[] = [];
+    const news: Promise<NewsItem | null>[] = [];
 
-    $rows.each((_, el) => {
+    for (const el of $rows) {
         const $el = $(el);
         const $a = $el.find("a.gameLink");
         const url = $a.attr("href");
         const gameName = $a.text().trim();
         const currentPlayers = $el.find("td:first-child .currentServers").text().trim();
 
-        if (url && gameName && currentPlayers) {
-            const title = gameName;
-            news.push({
-                url,
-                title,
-                id: url,
-                pubDate: Date.now(),
-                extra: {
-                    info: currentPlayers,
-                },
-            });
-        }
-    });
+        if (!url || !gameName || !currentPlayers) continue;
 
-    return news;
+        news.push(
+            (async () => {
+                const hashId = await generateUrlHashId(url);
+                const title = gameName;
+                return {
+                    id: hashId,
+                    title,
+                    url,
+                    pubDate: Date.now(),
+                    extra: {
+                        info: currentPlayers,
+                    },
+                } as NewsItem;
+            })()
+        );
+    }
+
+    const results = await Promise.all(news);
+    return results as NewsItem[];
 });
