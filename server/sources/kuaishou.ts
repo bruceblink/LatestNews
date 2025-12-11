814c00d5499a73fa7e3914bf3467766620503cce
likanug <likanug.g@qq.com>
2025-12-11 13:50:13 +0800

    refactor: 更新"kuaishou"数据源的实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/kuaishou.ts
+++ server/sources/kuaishou.ts
@@ -1,6 +1,6 @@
 import { myFetch } from "../utils/fetch";
 import { proxyPicture } from "../utils/proxy";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 interface KuaishouRes {
     defaultClient: {
@@ -46,22 +46,26 @@ export default defineSource(async () => {
     // 获取热榜列表数据
     const hotRankData = data.defaultClient[hotRankId] as HotRankData;
     // 转换数据格式
-    return hotRankData.items
-        .filter((k) => data.defaultClient[k.id].tagType !== "置顶")
-        .map((item) => {
-            // 从id中提取实际的热搜词
-            const hotSearchWord = item.id.replace("VisionHotRankItem:", "");
+    return await Promise.all(
+        hotRankData.items
+            .filter((k) => data.defaultClient[k.id].tagType !== "置顶")
+            .map(async (item) => {
+                // 从id中提取实际的热搜词
+                //const hotSearchWord = item.id.replace("VisionHotRankItem:", "");
 
-            // 获取具体的热榜项数据
-            const hotItem = data.defaultClient[item.id];
+                // 获取具体的热榜项数据
+                const hotItem = data.defaultClient[item.id];
+                const fulUrl = `https://www.kuaishou.com/search/video?searchKey=${encodeURIComponent(hotItem.name)}`;
+                const hashId = await generateUrlHashId(fulUrl);
 
-            return {
-                id: hotSearchWord,
-                title: hotItem.name,
-                url: `https://www.kuaishou.com/search/video?searchKey=${encodeURIComponent(hotItem.name)}`,
-                extra: {
-                    icon: hotItem.iconUrl && proxyPicture(hotItem.iconUrl),
-                },
-            };
-        });
+                return {
+                    id: hashId,
+                    title: hotItem.name,
+                    url: fulUrl,
+                    extra: {
+                        icon: hotItem.iconUrl && proxyPicture(hotItem.iconUrl),
+                    },
+                };
+            })
+    );
 });
