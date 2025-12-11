80827da5c3868292de2a9727e075cd92ad72f658
likanug <likanug.g@qq.com>
2025-12-11 14:25:12 +0800

    refactor: 更新数据源"smzdm"实现，使用generateUrlHashId()函数生成唯一的news id代替原来的id

--- server/sources/smzdm.ts
+++ server/sources/smzdm.ts
@@ -3,23 +3,30 @@ import type { NewsItem } from "@shared/types";
 import * as cheerio from "cheerio";
 
 import { myFetch } from "../utils/fetch";
-import { defineSource } from "../utils/source";
+import { defineSource, generateUrlHashId } from "../utils/source";
 
 export default defineSource(async () => {
     const baseURL = "https://post.smzdm.com/hot_1/";
     const html: any = await myFetch(baseURL);
     const $ = cheerio.load(html);
     const $main = $("#feed-main-list .z-feed-title");
-    const news: NewsItem[] = [];
-    $main.each((_, el) => {
+    const news: Promise<NewsItem | null>[] = [];
+    for (const el of $main) {
         const a = $(el).find("a");
         const url = a.attr("href")!;
         const title = a.text();
-        news.push({
-            url,
-            title,
-            id: url,
-        });
-    });
-    return news;
+        news.push(
+            (async () => {
+                const hashId = await generateUrlHashId(url);
+
+                return {
+                    id: hashId,
+                    url,
+                    title,
+                } as NewsItem;
+            })()
+        );
+    }
+    const results = await Promise.all(news);
+    return results as NewsItem[];
 });
